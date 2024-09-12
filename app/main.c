fc84ae2182ecf8170db2ea70887dffaa95674c3b
Robbie Gill <34260042+robbieGill4096@users.noreply.github.com>
2024-09-12 18:42:00 +0000

    added some broken starter code

--- app/main.c
+++ app/main.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+//#include <type.h>
+#include <unistd.h>
 
 int main(void)
 {
-  printf("hello world\n");
+  printf("hellod world\n");
+  int c;
+
+  while ((c=getopt(argc,argv, "abc:")) != -1)
+    switch(c){
+
+      case 'a':
+        printf("get a here");
+        break;
+      case 'b':
+        printf("get b here");
+        break;
+      case 'c':
+        printf("get c here");
+        break;
+      case 'd':
+        printf("get d here");
+        break;
+      case '?':
+        printf("? here");
+        break;
+    }
+
   return 0;
 }
