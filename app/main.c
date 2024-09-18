094a9051c32dd123c282cee4ce1d623c9c7dd215
Robbie Gill <34260042+robbieGill4096@users.noreply.github.com>
2024-09-18 17:27:45 +0000

    Added the $ terminal loop

--- app/main.c
+++ app/main.c
@@ -3,7 +3,9 @@
 //#include <type.h>
 #include <unistd.h>
 #include <lab.h>
-
+//step 4 code
+#include <readline/readline.h>
+#include <readline/history.h>
 void print_version() {
     printf("Shell version %d.%d\n", lab_VERSION_MAJOR, lab_VERSION_MINOR);
 }
@@ -19,6 +21,7 @@ int main(int argc, char *argv[])
 
       case 'v':
         print_version();
+        
         break;
       case 'b':
         printf("get b here");
@@ -39,5 +42,13 @@ int main(int argc, char *argv[])
 
     //catch case of invalid flags
 
+  //
+  char *line;
+  using_history();
+  while ((line=readline("$"))){
+      printf("%s\n",line);
+      add_history(line);
+      free(line);
+  }
   return 0;
 }
