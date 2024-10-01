bb78364693877fff18c1066efe843c16d3130a33
Robbie Gill <34260042+robbieGill4096@users.noreply.github.com>
2024-10-01 03:59:51 +0000

    added cd implimented lab.c

--- app/main.c
+++ app/main.c
@@ -3,25 +3,39 @@
 //#include <type.h>
 #include <unistd.h>
 #include <lab.h>
+#include "../src/lab.h"
 //step 4 code
 #include <readline/readline.h>
 #include <readline/history.h>
 void print_version() {
     printf("Shell version %d.%d\n", lab_VERSION_MAJOR, lab_VERSION_MINOR);
 }
+void retr_cwd() {
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("Current directory: %s\n", cwd);  // Print the current working directory
+    } else {
+        perror("getcwd");
+    }
+}
 
 int main(int argc, char *argv[])
 {
   //printf("hellod world\n");
-  
+ // char *my_prompt = getenv("MY_PROMPT"); //added to grab the user prompt Task5
+
+  //if (my_prompt == NULL) {
+  //      my_prompt = "Shell>";  // Default prompt
+   // }
+  char *my_prompt = get_prompt("MY_PROMPT");
+
   int opt;
 
   while ((opt=getopt(argc,argv, "vbc:")) != -1)
     switch(opt){
 
       case 'v':
-        print_version();
-        
+        print_version(); 
         break;
       case 'b':
         printf("get b here");
@@ -36,7 +50,7 @@ int main(int argc, char *argv[])
         printf("? here");
         break;
       default:
-        printf("default case");
+        //printf("default case");
         break;
     }
 
@@ -45,10 +59,87 @@ int main(int argc, char *argv[])
   //
   char *line;
   using_history();
-  while ((line=readline("$"))){
-      printf("%s\n",line);
+
+  while ((line=readline(my_prompt))){ // change so that reads getEnv to select the symbol or string
+      
+      //user command prompts go here
+      //Task 6
+      //Exit
+      //cd -use env for current directory
+      //history command
+
+     if (strncmp(line, "exit", 4) == 0) {
+            int exit_status = 0;  // Default exit status is 0 (normal exit)
+
+            // Need case for non-normal exit nonzero
+
+            free(line);  // Clean up before exiting
+            printf("freeing prompt");
+            free(my_prompt);
+            return exit_status;
+        }
+      if (strncmp(line, "pwd", 3) == 0) {
+            
+            char cwd[1024];
+                if (getcwd(cwd, sizeof(cwd)) != NULL) {
+                    printf("%s\n", cwd);
+                } else {
+                    perror("getcwd() error");
+                }
+            
+            
+        }
+      if (strncmp(line, "history", 7) == 0) {
+           
+
+            printf("history");
+           
+            
+        }
+
+      if (strncmp(line, "cd", 2) == 0) {
+
+        char *dir = strtok(line, " ");
+        dir = strtok(NULL, " ");  // Get the directory
+           
+        change_dir(dir);
+     
+
+          //      //struct passwd *pw = getpwuid(getuid());
+          //          // dir = pw->pw_dir;
+          //           //dir = pw->pw_dir;
+          //     //printf("no argumetn passed%s,",dir);
+              
+          //   }
+          //   //check for valid path? 
+          //   if (chdir(dir) != 0) {
+          //       //perror("cd");
+          //       chdir(dir);
+          //   } else { //the directory call is valid so now we execute
+
+          //      char cwd[1024];
+          //       if (getcwd(cwd, sizeof(cwd)) != NULL) {
+          //           printf("Changed directory to: %s\n", cwd);
+          //       } else {
+          //           perror("getcwd() error");
+          //       }
+
+          //   }
+
+
+
+          //   //if second argument found change to taht directory 
+
+
+        }
+     
+      
+      //printf("%s\n",line);
       add_history(line);
+      //printf("%s\n",cwd);
       free(line);
+      
   }
+  printf("freeing prompt");
   return 0;
 }
--- src/lab.c
+++ src/lab.c
@@ -1,1 +1,245 @@
 /**Update this file with the starter code**/
+#include <string.h>
+#include "../src/lab.h"
+#include <errno.h>
+
+// char **cmd_parse(char const *line){
+    
+// }
+
+// void cmd_free(char **line){
+//     for(int i = 0; i < 10; i++){
+//         if(line[i] != NULL){
+//             free(line[i]);
+//         }
+//     }
+//     free(line);
+// }
+
+// char *trim_white(char *line){
+    
+//     return line;
+// }
+   /**
+   * @brief Set the shell prompt. This function will attempt to load a prompt
+   * from the requested environment variable, if the environment variable is
+   * not set a default prompt of "shell>" is returned.  This function calls
+   * malloc internally and the caller must free the resulting string.
+   *
+   * @param env The environment variable
+   * @return const char* The prompt
+   */
+ char *get_prompt(const char *env){
+    //     char *prompt = getenv(env);
+    //      if (prompt == NULL) {
+    //     prompt = "shell>";  // Default prompt
+    //     return prompt;
+    // }
+        
+    // return prompt;
+    char *prompt = getenv(env);
+    //check if no custom shell design is given
+    if(prompt == NULL){
+        //default shell design
+        //dynamically allocate memory because of the test file
+        prompt = strdup("shell>");
+        //should i check for an allocation failure?
+    }
+    else {
+
+        prompt = strdup(prompt);
+        return prompt;
+    }
+
+ }
+
+/**
+   * Changes the current working directory of the shell. Uses the linux system
+   * call chdir. With no arguments the users home directory is used as the
+   * directory to change to.
+   *
+   * @param dir The directory to change to
+   * @return  On success, zero is returned.  On error, -1 is returned, and
+   * errno is set to indicate the error.
+   */
+ 
+int change_dir(char **dir) {
+    //dir = strtok(NULL, " "); 
+   //printf(dir);
+    //case to get home no args
+          if (!dir){ 
+            //get env then cd home
+            const char *home = getenv("HOME");
+
+            //check if getting home path failed
+            if (home == NULL) {
+            struct passwd *pw = getpwuid(getuid());
+            //home = pw->pw_dir;
+           
+            }
+
+
+            chdir(home); // Attempt to change to home directory
+            return 0;
+            //if that fails now call getpwuid
+
+          }
+          //case where args
+          else { 
+
+            //printf("args case");
+           //try to run args if it fails return error
+           if (chdir(dir) != 0 ) {
+              errno = ENOENT;
+              return -1;}
+
+           else{
+              //succsffully change to a dir
+              //chdir(*dir);
+              return 0; 
+              }
+           }
+           
+}
+
+
+  /**
+   * @brief Convert line read from the user into to format that will work with
+   * execvp. We limit the number of arguments to ARG_MAX loaded from sysconf.
+   * This function allocates memory that must be reclaimed with the cmd_free
+   * function.
+   *
+   * @param line The line to process
+   *
+   * @return The line read in a format suitable for exec
+   */
+  char **cmd_parse(char const *line){
+    long ARG_MAX = sysconf(_SC_ARG_MAX);
+
+    // Allocate memory for the argument list
+    char **cmd = malloc(ARG_MAX * sizeof(char *));
+    if (cmd == NULL) {
+        
+        return NULL;
+    }
+
+    // Make a writable copy of the line
+    char *line_copy = strdup(line);
+    if (line_copy == NULL) {
+        
+        return NULL;
+    }
+
+    // Use the writable copy for tokenization
+    char *token = strtok(line_copy, " ");
+    int i = 0;
+    while (token != NULL) {
+        cmd[i] = strdup(token);  // Duplicate each token
+        token = strtok(NULL, " ");
+        i++;
+    }
+    cmd[i] = NULL;  // Null-terminate the argument array
+
+    free(line_copy);  // Free the copy of the input line after tokenization
+    return cmd;
+}
+
+
+  /**
+   * @brief Free the line that was constructed with parse_cmd
+   *
+   * @param line the line to free
+   */
+
+  void cmd_free(char **line) {
+    if (line == NULL) {
+        return;  // No action needed if the line is already NULL
+    }
+
+    // Free each string in the array
+    for (int i = 0; line[i] != NULL; i++) {
+        free(line[i]);  // Free each duplicated token
+    }
+
+    // Free the array itself
+    free(line);
+}
+
+/**
+   * @brief Trim the whitespace from the start and end of a string.
+   * For example "   ls -a   " becomes "ls -a". This function modifies
+   * the argument line so that all printable chars are moved to the
+   * front of the string
+   *
+   * @param line The line to trim
+   * @return The new line with no whitespace
+   */
+  char *trim_white(char *line){
+
+    // Trim leading whitespace
+    while (*line != '\0' && isspace(*line)) {
+        line++;
+    }
+
+    // Trim trailing whitespace
+    char *end = line + strlen(line) - 1;
+    while (end > line && isspace(*end)) {
+        end--;
+    }
+    end[1] = '\0';  // Null-terminate the string
+
+    return line;
+  }
+
+
+  /**
+   * @brief Takes an argument list and checks if the first argument is a
+   * built in command such as exit, cd, jobs, etc. If the command is a
+   * built in command this function will handle the command and then return
+   * true. If the first argument is NOT a built in command this function will
+   * return false.
+   *
+   * @param sh The shell
+   * @param argv The command to check
+   * @return True if the command was a built in command
+   */
+  bool do_builtin(struct shell *sh, char **argv){
+    return true;
+
+  }
+
+  /**
+   * @brief Initialize the shell for use. Allocate all data structures
+   * Grab control of the terminal and put the shell in its own
+   * process group. NOTE: This function will block until the shell is
+   * in its own program group. Attaching a debugger will always cause
+   * this function to fail because the debugger maintains control of
+   * the subprocess it is debugging.
+   *
+   * @param sh
+   */
+  void sh_init(struct shell *sh);
+
+  /**
+   * @brief Destroy shell. Free any allocated memory and resources and exit
+   * normally.
+   *
+   * @param sh
+   */
+  void sh_destroy(struct shell *sh){
+
+
+  }
+
+  /**
+   * @brief Parse command line args from the user when the shell was launched
+   *
+   * @param argc Number of args
+   * @param argv The arg array
+   */
+  void parse_args(int argc, char **argv){
+    //parse the arguments
+
+  }
+
+
